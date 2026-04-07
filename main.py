2e63a385c654607fffd4c76ea8bd3fad4b2a2de0
Matthew Eichelman <meichelman5@gmail.com>
2026-04-07 17:06:51 -0500

    Testing

--- main.py
+++ main.py
@@ -5,28 +5,26 @@ from make_mutationrate import make_mutation_rate
 import numpy as np
 
 
-def train(obs_file, mutrates_file, out_file, window_size):
+def train(obs_file, mutrates_file, out_file):
     hmm_parameters = read_HMM_parameters_from_file(None)
-    obs, mutrates = load_obs_mut(obs_file, mutrates_file, window_size)
+    obs, mutrates = load_obs_mut(obs_file, mutrates_file)
 
     print('-' * 40)
     print(f'> number of windows: {len(obs)}. Number of kmers = {obs.astype(np.int64).sum()}')
     print('> output is', out_file) 
-    print('> window size is', window_size, 'bp') 
     print('-' * 40)
 
-    hmm_parameters = TrainModel(obs, mutrates, hmm_parameters, window_size)
+    hmm_parameters = TrainModel(obs, mutrates, hmm_parameters)
     write_HMM_to_file(hmm_parameters, out_file)
     
 
-def decode(obs_file, mutrates_file, param_file, out_file, window_size):
+def decode(obs_file, mutrates_file, param_file, out_file):
     hmm_parameters = read_HMM_parameters_from_file(param_file)
-    obs, mutrates = load_obs_mut(obs_file, mutrates_file, window_size)
+    obs, mutrates = load_obs_mut(obs_file, mutrates_file)
 
     print('-' * 40)
     print(f'> number of windows: {len(obs)}. Number of kmers = {obs.astype(np.int64).sum()}')
     print('> output is', out_file)
-    print('> Window size is', window_size, 'bp')
     print('> Decode with posterior decoding')
     print('-' * 40)
     
@@ -35,7 +33,7 @@ def decode(obs_file, mutrates_file, param_file, out_file, window_size):
     pmap_path = PMAP_path(posterior_probs)
     viterbi_path = Viterbi_path(emissions, hmm_parameters)
     
-    Write_posterior_probs(obs, mutrates, posterior_probs, pmap_path, viterbi_path, hmm_parameters, out_file, window_size)
+    Write_posterior_probs(obs, mutrates, posterior_probs, pmap_path, viterbi_path, hmm_parameters, out_file, window_size = 2000)
 
 
 def main():
@@ -62,14 +60,14 @@ def main():
 
     
     elif mode == 'train':
-        # python3 main.py train obs.bed mutrates.txt window_size
+        # python3 main.py train obs.bed mutrates.txt
 
         if len(args) < 5:
             sys.exit('\n\nMust input:\n\tObservations\n\tMutation rate\n\n')
-        obs, mutrates, window_size = args[2], args[3], int(args[4])
+        obs, mutrates = args[2], args[3]
         out = 'trained.json'
         
-        train(obs, mutrates, out, window_size)
+        train(obs, mutrates, out)
     
     elif mode == 'decode':
         # python3 main.py decode obs.bed mutrates.txt trained.json
@@ -77,10 +75,10 @@ def main():
         if len(args) < 6:
             sys.exit('\n\nMust input:\n\tParameters\n\tObservations\n\tMutation rate\n\tOutput\n\n')
         
-        obs, mutrates, params, window_size = args[2], args[3], args[4], int(args[5])
+        obs, mutrates, params = args[2], args[3], args[4]
         out = 'probs_and_path.tsv'
         
-        decode(obs, mutrates, params, out, window_size)
+        decode(obs, mutrates, params, out)
 
 
 if __name__ == "__main__":
